e613a8d09c271a2991cc27ad62929108f60b3a8a
felipesousafs <felipe.sousa.fs@gmail.com>
2017-02-07 12:29:55 -0300

    Corrigido erro NoReverseMatch no PythonAnywhere.com

--- engsoft/models.py
+++ engsoft/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils import timezone
 
 class Projeto(models.Model):
 	nome_produto = models.CharField(max_length=200)
--- engsoft/urls.py
+++ engsoft/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index),
-	url(r'^list$', views.list),
+	url(r'^$', views.index, name="index"),
+	url(r'^list$', views.list, name="list"),
 	url(r'^new$', views.new_project, name='new_project'),
 ]
